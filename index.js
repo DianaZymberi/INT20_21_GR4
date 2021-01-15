27880d800a7bd74e1518355bb95576f0c8aa8551
Fortesa Mujaj <55389120+fortesamujaj@users.noreply.github.com>
2021-01-15 08:10:18 +0100

    added two projectiles

--- index.js
+++ index.js
@@ -35,6 +35,12 @@ class Projectile{
         c.fillStyle = this.color
         c.fill()
     }
+
+    update(){
+        this.draw()
+        this.x = this.x + this.velocity.x
+        this.y = this.y + this.velocity.y
+    }
 }
 
 const x = canvas.width / 2
@@ -43,9 +49,39 @@ const y = canvas.height / 2
 const player = new Player(x, y, 30, 'blue')
 player.draw()
 
+const projectile = new Projectile(
+    canvas.width / 2,
+    canvas.height / 2,
+    5,
+    'red',
+    {
+        x: 1,
+        y: 1
+    }
+ )
+
+ const projectile2 = new Projectile(
+    canvas.width / 2,
+    canvas.height / 2,
+    5,
+    'green',
+    {
+        x: -1,
+        y: -1
+    }
+ )
+
+
+const projectiles = [projectile, projectile2]
+
+function animate(){
+    requestAnimationFrame(animate)
+    projectiles.forEach((projectile) =>{
+        projectile.update()
+    })
+}
+
 addEventListener('click', (event) => {
-    const projectile = new Projectile(
-        event.clientX, event.clientY, 5, 'red', null
-    )
-    projectile.draw()
 })
+
+animate()
