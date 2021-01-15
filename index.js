16295ee8844189920db56789d699493a8b0ffeae
Fortesa Mujaj <55389120+fortesamujaj@users.noreply.github.com>
2021-01-15 15:41:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -73,19 +73,24 @@ class Particle{
         this.radius = radius
         this.color = color
         this.velocity = velocity
+        this.alpha = 1
     }
 
     draw(){
+        c.save()
+        c.globalAlpha = this.alpha
         c.beginPath()
         c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
         c.fillStyle = this.color
         c.fill()
+        c.restore()
     }
 
     update(){
         this.draw()
         this.x = this.x + this.velocity.x
         this.y = this.y + this.velocity.y
+        this.alpha -= 0.01
     }
 }
 
@@ -131,8 +136,13 @@ function animate(){
     c.fillStyle = 'rgba(0, 0, 0, 0.1)'
     c.fillRect(0, 0, canvas.width, canvas.height)
     player.draw()
-    particles.forEach(particle => {
-        particle.update()
+    particles.forEach((particle, index) => {
+        if(particle.alpha <= 0){
+            particles.splice(index, 1)
+        }
+        else{
+            particle.update()
+        }
     });
 
     projectiles.forEach((projectile, index) => {
@@ -169,9 +179,10 @@ function animate(){
                 // when projectiles touch enemy
             if(dist - enemy.radius - projectile.radius < 1) 
             {
-                for (let i=0; i<8; i++){
+                // create explosions
+                for (let i=0; i< enemy.radius * 2; i++){
                     particles.push(
-                        new Particle(projectile.x, projectile.y, 3, enemy.color, {
+                        new Particle(projectile.x, projectile.y, Math.random() * 2, enemy.color, {
                         x: Math.random() - 0.5, y: Math.random() - 0.5
                         })
                     )
